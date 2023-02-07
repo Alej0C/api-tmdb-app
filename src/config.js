bbd7fa85b89c81cf097601044272e1d5f7bcd6f8
Alejandro <hansalejandroca@gmail.com>
2023-02-07 11:15:47 -0500

    final commit

---
+++ src/config.js
@@ -0,0 +1,6 @@
+const BASE_URL = "https://api.themoviedb.org/3/";
+const API_KEY_PARAM = "?api_key=80e1187cab8af883bf9163ca7c4a38bf";
+const BACKDROP_BASE_URL = "https://image.tmdb.org/t/p/original";
+const SMALL_IMG_COVER_BASE_URL = "https://image.tmdb.org/t/p/w300";
+
+export { BASE_URL, API_KEY_PARAM, BACKDROP_BASE_URL, SMALL_IMG_COVER_BASE_URL };
